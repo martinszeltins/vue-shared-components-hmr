1a460eff47e1ee89f77e3bf73b6806cfdbf0d4ec
Martins <martins.zeltins@arkbauer.com>
2022-05-11 08:51:46 +0300

    More shared resources

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -1,9 +1,21 @@
 <template>
-    <div>This component comes from a shared repo</div>
+    <div class="m-4">
+        <div>This component comes from a shared repo:</div>
 
-    <MySharedComponent msg="Message Prop!" />
+        <MySharedComponent msg="Message Prop!" />
+
+        <p class="font-semibold mt-4">Shared service:</p>
+        <p>Mouse position is at: {{ x }}, {{ y }}</p>
+
+        <p class="font-semibold mt-4">Shared image:</p>
+        <img :src="sharedImage" alt="Shared image" width="100" height="100" />
+    </div>
 </template>
 
 <script setup>
+    import sharedImage from 'shared/src/assets/dolphin.png'
+    import { useMouse } from 'shared/src/services/useMouse.js'
     import MySharedComponent from 'shared/src/components/MySharedComponent.vue'
+
+    const { x, y } = useMouse()
 </script>
