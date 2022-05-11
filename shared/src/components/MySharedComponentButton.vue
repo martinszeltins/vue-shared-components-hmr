7d3c2f1c7d3988cbfae39372ff75b83ed3dd7d84
Martins <martins.zeltins@arkbauer.com>
2022-05-11 08:52:45 +0300

    Cleanup

--- shared/src/components/MySharedComponentButton.vue
+++ shared/src/components/MySharedComponentButton.vue
@@ -2,6 +2,10 @@
     Button with 3rd party tooltip component <br><br>
 
     <div>
-      <button class="bg-gray-200 rounded px-2 py-1" v-tooltip="'You have new messages.'">Button</button>
+        <button
+            class="bg-gray-200 rounded px-2 py-1"
+            v-tooltip="'You have new messages.'">
+            Button
+        </button>
     </div>
 </template>
